620fe2d0e1d5fb7d2ceb54601185169b18e94c47
Duilio Luca Bacocco <duiliol.b@gmail.com>
2020-06-19 10:09:39 +0200

    assignment 3 bridge is fully functional. Note that configuration is in ttn_bridge_conf.py which is not being loaded on the repo.

--- ttn_mqtt_bridge/bridge.py
+++ ttn_mqtt_bridge/bridge.py
@@ -1,72 +1,63 @@
-from paho.mqtt import client as mqtt
+import base64
+import json
 import time
+from datetime import datetime
+from ttn_mqtt_bridge.ttn_bridge_conf import *
 
-CONNECTION_WAIT_SLEEP = 5
-MQTT_BROKER_ADDRESS = "localhost"
-MQTT_TOPIC = "stations/sensor_values"
+from paho.mqtt import client as mqtt, publish
 
-TTN_BROKER_ADDRESS=""
-TTN_TOPIC = "+/devices/+/up"
-TTN_USERNAME_APP_ID = ""
-TTN_PW_ACCESS_KEY = ""
+mqtt_broker = mqtt.Client("TTN Bridge")
 
-mqtt_client = mqtt.Client("Bridge")
-
-def mqtt_on_connect(client, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):
     global connected  # Use global variable
     connected = False
     if rc == 0:
-        print("Connected to Broker")
+        print("Connected to TTN Broker")
+        connected = True  # Signal connection
+        print("Subscribing to "+TTN_TOPIC+" topic")
+        client.subscribe(TTN_TOPIC, 1)
+    else:
+        print("Connection to TTN Broker failed")
+
+def mqtt_broker_on_connect(client, userdata, flags, rc):
+    global connected  # Use global variable
+    connected = False
+    if rc == 0:
+        print("Connected to MQTT Broker")
         connected = True  # Signal connection
     else:
         print("Connection to MQTT Broker failed")
 
-def ttn_on_connect(mqtt_client, userdata, flags, rc):
-    global connected  # Use global variable
-    connected = False
-    if rc == 0:
-        print("Connected to TTN MQTT Broker")
-        connected = True  # Signal connection
-        print("Subscribing to "+TTN_TOPIC+" topic")
-        mqtt_client.subscribe(TTN_TOPIC, 1)
-    else:
-        print("Connection to Broker failed")
-
-def mqtt_on_message(client, userdata, message):
-    print("MQTT Message received: " + message.topic + " - " + message.payload)
-
-def ttn_on_message(client, userdata, message):
-    print("MQTT Message received from TTN: " + message.topic + " - " + message.payload)
-    #mqtt_client.publish(MQTT_TOPIC, m, 1)
+def on_message(client, userdata, message):
+    print("Message received: " + message.topic + " - " + message.payload.decode())
+    recv_json = json.loads(message.payload.decode())
+    payload_decode = base64.b64decode(recv_json["payload_raw"])
+    payload_json = json.loads(payload_decode)
+    new_timestamp = datetime.now()
+    payload_json["time"]=new_timestamp.isoformat()
+    print("Forwarding to MQTT broker...")
+    print(MQTT_TOPIC + ": "+ json.dumps(payload_json))
+    publish.single(
+        MQTT_TOPIC, payload=json.dumps(payload_json), qos=1, retain=False,
+        hostname=BROKERADDRESS, port=1883, client_id='TTN Bridge',
+        keepalive=60, will=None, auth=None, tls=None, protocol=mqtt.MQTTv311)
+    print("Forward completed.")
 
 def main():
-    print("--TTN -> MQTT bridge initialization--")
+    #f = open("received_values.txt","w")
+    #f.close()
+    print("Initializing Mosquitto Client")
+    mqtt_client = mqtt.Client("TTN Bridge")
+    mqtt_client.username_pw_set(TTN_USERNAME_APP_ID,TTN_PW_ACCESS_KEY)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
 
-    print("Initializing MQTT protocol for TTN")
-    ttn_mqtt_client=mqtt.Client("Bridge")
-    ttn_mqtt_client.on_connect = ttn_on_connect
-    ttn_mqtt_client.on_message = ttn_on_message
+    print("Connecting to Broker")
+    mqtt_client.connect(TTN_BROKER_ADDRESS)
 
-    print("Connecting to TTN MQTT Broker at " + TTN_BROKER_ADDRESS)
-    ttn_mqtt_client.connect(TTN_BROKER_ADDRESS)
-    ttn_mqtt_client.loop_start()
+    print("Listening...")
+    mqtt_client.loop_forever()
 
-    print("Waiting for TTN connection to complete")
-    time.sleep(CONNECTION_WAIT_SLEEP)
-
-    print("Initializing MQTT protocol")
-    mqtt_client.on_connect = mqtt_on_connect
-    mqtt_client.on_message = mqtt_on_message
-
-    print("Connecting to MQTT Broker at " + MQTT_BROKER_ADDRESS)
-    mqtt_client.connect(MQTT_BROKER_ADDRESS)
-    mqtt_client.loop_start()
-
-    print("Waiting for connection to complete")
-    time.sleep(CONNECTION_WAIT_SLEEP)
-
-    print("Listening for TTN messages...")
-    ttn_mqtt_client.loop_forever()
 
 if __name__ == "__main__":
     main()
